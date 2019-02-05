e4ce63af61c065526f4b5a9e3ab68903218b4447
dongwhan1 <lionfish0418@gmail.com>
2019-02-05 21:32:59 +0900

    ..

--- bcms/models.py
+++ bcms/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    table = models.CharField(max_length=20)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
